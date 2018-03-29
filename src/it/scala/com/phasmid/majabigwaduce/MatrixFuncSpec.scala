44abe49ed8905762725de129ded8c6f86f2532c7
Scala Prof <scalaprof@gmail.com>
2018-03-29 00:35:21 -0400

    Implemented Matrix multiplication

---
+++ src/it/scala/com/phasmid/majabigwaduce/MatrixFuncSpec.scala
@@ -0,0 +1,47 @@
+package com.phasmid.majabigwaduce
+
+import akka.actor.ActorSystem
+import akka.event.LoggingAdapter
+import akka.util.Timeout
+import com.phasmid.majabigwaduce.examples.matrix.MatrixOperation
+import com.typesafe.config.{Config, ConfigFactory}
+import org.scalamock.scalatest.MockFactory
+import org.scalatest._
+import org.scalatest.concurrent._
+import org.scalatest.time.{Seconds, Span}
+
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
+
+class MatrixFuncSpec extends FlatSpec with Matchers with Futures with ScalaFutures with Inside with MockFactory {
+  "matrix" should "work" in {
+    val configRoot = ConfigFactory.load
+    implicit val config: Config = configRoot.getConfig("Matrix")
+    implicit val system: ActorSystem = ActorSystem(config.getString("name"))
+    implicit val to: Timeout = getTimeout(config.getString("timeout"))
+    val rows = config.getInt("rows")
+    val cols = config.getInt("columns")
+    implicit val logger: LoggingAdapter = system.log
+    import ExecutionContext.Implicits.global
+
+    val op: MatrixOperation[Int] = MatrixOperation(x => x % 10)
+    val matrix = Seq(Seq(1, 1), Seq(2, 1))
+    val vector = Seq(3, 5)
+    val isf: Future[Seq[Int]] = op(matrix, vector)
+
+    whenReady(isf, timeout(Span(300, Seconds))) {
+      is: Seq[Int] => assert(is.head == 8 && is.tail.head == 11)
+    }
+  }
+
+  def getTimeout(t: String): Timeout = {
+    val durationR = """(\d+)\s*(\w+)""".r
+    t match {
+      case durationR(n, s) => new Timeout(FiniteDuration(n.toLong, s))
+      case _ => Timeout(10 seconds)
+    }
+  }
+
+}
+
+
---
+++ src/main/scala/com/phasmid/majabigwaduce/examples/matrix/Matrix.scala
@@ -0,0 +1,65 @@
+package com.phasmid.majabigwaduce.examples.matrix
+
+import akka.actor.ActorSystem
+import akka.event.LoggingAdapter
+import akka.util.Timeout
+import com.phasmid.majabigwaduce._
+import com.phasmid.majabigwaduce.examples.CountWords.getTimeout
+
+import scala.concurrent.duration._
+import com.typesafe.config.{Config, ConfigFactory}
+
+import scala.collection.immutable
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.Random
+
+case class MatrixOperation[X: Numeric](keyFunc: Int=>Int)(implicit system: ActorSystem, logger: LoggingAdapter, config: Config, timeout: Timeout, ec: ExecutionContext) extends ((Seq[Seq[X]],Seq[X]) => Future[Seq[X]]) {
+
+  override def apply(xss: Seq[Seq[X]], vector: Seq[X]): Future[Seq[X]] = {
+    val xn = implicitly[Numeric[X]]
+
+    def f(i: Int, xs: Seq[X]): (Int, X) = (keyFunc(i), Matrix.dot(xs, vector))
+    def g(a: X, x: X) = xn.plus(a,x)
+        val s1: MapReduce[(Int, Seq[X]), Int,X] = MapReducePipe[Int,Seq[X],Int,X,X](
+          f,
+          g,
+          1
+        )
+    val r: Reduce[Int, X, Seq[X]] = Reduce[Int, X, Seq[X]](init _)(addElement)
+    val mr: ASync[Seq[(Int, Seq[X])], Seq[X]] = s1 | r
+
+    mr((xss zipWithIndex) map { t: (Seq[X], Int) => t swap })
+  }
+
+  private def init = Seq[X]()
+
+  private def addElement(x: Seq[X], y: X) = y +: x
+}
+
+object Matrix extends App {
+
+  def dot[X: Numeric](as: Seq[X], bs: Seq[X]): X = {
+    val xn = implicitly[Numeric[X]]
+    def product(ab: (X,X)): X = xn.times(ab._1,ab._2)
+    ((as zip bs) map product).sum
+  }
+
+  val configRoot = ConfigFactory.load
+  implicit val config: Config = configRoot.getConfig("Matrix")
+  implicit val system: ActorSystem = ActorSystem(config.getString("name"))
+  implicit val timeout: Timeout = getTimeout(config.getString("timeout"))
+  val rows = config.getInt("rows")
+  val cols = config.getInt("columns")
+  val modulus = config.getInt("modulus")
+  implicit val logger: LoggingAdapter = system.log
+  import ExecutionContext.Implicits.global
+
+  val op: MatrixOperation[Double] = MatrixOperation(x => x % modulus)
+  def row(i: Int): Seq[Double] = {val r = new Random(i); (Stream.from(0) take cols) map (x => r.nextDouble())}
+  val matrix: Seq[Seq[Double]] = Stream.tabulate(rows)(row)
+  val vector: Seq[Double] = row(-1)
+  val isf: Future[Seq[Double]] = op(matrix,vector)
+  Await.result(isf, 10.minutes)
+  isf foreach (println)
+  system.terminate()
+}
