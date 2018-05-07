194bd3945f9dae3d9dfd57a94a82ce0169d7eb53
Scala Prof <scalaprof@gmail.com>
2018-05-07 16:07:38 +0100

    Added EagerDD to DataDefinition

--- src/main/scala/com/phasmid/majabigwaduce/DataDefinition.scala
+++ src/main/scala/com/phasmid/majabigwaduce/DataDefinition.scala
@@ -6,6 +6,7 @@ package com.phasmid.majabigwaduce
 
 import akka.actor.ActorSystem
 import akka.util.Timeout
+import com.phasmid.majabigwaduce.LazyDD.joinMap
 import com.typesafe.config.{Config, ConfigFactory}
 
 import scala.concurrent.duration.FiniteDuration
@@ -15,7 +16,7 @@ import scala.concurrent.{ExecutionContext, Future}
   * Trait to represent a "data definition" (similar to RDD in Spark).
   * Essentially, a DataDefinition[K, V] is a function which transforms Unit into a Future[Map[K,V].
   * Like RDD, it is lazy and can be partitioned.
-  * In order to yield a concrete value, there are two methods which may be called: apply() and aggregate(f).
+  * In order to yield a concrete value, i.e. an "action", there are three methods which may be called: apply(), reduce(f), and count.
   *
   * Created by scalaprof on 10/31/16.
   *
@@ -35,46 +36,91 @@ trait DataDefinition[K, V] extends (() => Future[Map[K, V]]) {
   def map[L, W: Monoid](f: ((K, V)) => (L, W)): DataDefinition[L, W]
 
   /**
-    * Evaluate this DataDefinition
+    * Method to evaluate this DataDefintion and reduce the dimensionality of the result by ignoring the keys
+    * and aggregating the values according to the function wv_w
     *
-    * @return a map of key-value pairs wrapped in Future
-    */
-  def apply(): Future[Map[K, V]]
-
-  /**
-    * Evaluate this DataDefintion but reduce the dimensionality of the result by ignoring the keys
-    * and aggregating the values according to the function f
-    *
-    * @param f the aggregation function
+    * @param wv_w the aggregation function
     * @tparam W the underlying type of the result
     * @return a W value, wrapped in Future.
     */
-  def aggregate[W: Zero](f: (W, V) => W): Future[W]
+  def reduce[W: Zero](wv_w: (W, V) => W): Future[W]
 
   /**
-    * Clean up any residual resources from this DataDefinition
+    * Evaluate the number of elements in this DataDefinition
+    *
+    * @return the number of k-v pairs
     */
-  def clean(): Unit
-
   def count: Future[Int]
 
-  def filter(f: ((K, V)) => Boolean): DataDefinition[K, V]
+  /**
+    * Method to filter this DataDefinition according to a predicate which takes a k-v tuple.
+    *
+    * @param p the predicate which will yield a Boolean for a given k-v tuple.
+    * @return a new DataDefinition containing only those k-v pairs which satisfy the predicate p.
+    */
+  def filter(p: ((K, V)) => Boolean): DataDefinition[K, V]
 
   /**
     * Join method to perform inner join.
     *
     * @param other the DataDefinition with which to join this
-    * @tparam M key type of other and also of the result
-    * @tparam X value type of other
+    * @tparam L key type of other and also of the result
+    * @tparam W value type of other
     * @return the inner join of this and other
     */
-  def join[M >: K, X: Monoid](other: DataDefinition[M, X]): DataDefinition[M, (V, X)]
+  def join[L >: K, W: Monoid](other: DataDefinition[L, W]): DataDefinition[L, (V, W)]
+
+  /**
+    * Clean up any residual resources from this DataDefinition
+    */
+  def clean(): Unit
+}
+
+case class EagerDD[K, V](kVm: Map[K, V])(implicit context: DDContext) extends BaseDD[K, V] {
+
+  private implicit val ec: ExecutionContext = context.ec
+
+  /**
+    * Evaluate this DataDefinition
+    *
+    * @return a map of key-value pairs wrapped in Future
+    */
+  override def apply(): Future[Map[K, V]] = Future(kVm)
+
+  /**
+    * Method to form a new DataDefinition where the resulting values derive from applying the function f to the original values
+    *
+    * @param f the function to transform key-value pairs
+    * @tparam L the underlying type of the keys of the resulting map
+    * @tparam W the underlying type of the values of the resulting map
+    * @return a new DataDefinition
+    */
+  def map[L, W: Monoid](f: ((K, V)) => (L, W)): DataDefinition[L, W] = EagerDD(for ((k, v) <- kVm) yield f(k, v))
+
+  /**
+    * Method to filter this DataDefinition according to a predicate which takes a k-v tuple.
+    *
+    * @param p the predicate which will yield a Boolean for a given k-v tuple.
+    * @return a new DataDefinition containing only those k-v pairs which satisfy the predicate p.
+    */
+  def filter(p: ((K, V)) => Boolean): DataDefinition[K, V] = EagerDD[K, V](kVm.filter(p))
+
+  /**
+    * Join method to perform inner join.
+    *
+    * @param other the DataDefinition with which to join this
+    * @tparam L key type of other and also of the result
+    * @tparam W value type of other
+    * @return the inner join of this and other
+    */
+  def join[L >: K, W: Monoid](other: DataDefinition[L, W]): DataDefinition[L, (V, W)] = other match {
+    case edd: EagerDD[L, W]@unchecked => EagerDD[L, (V, W)](joinMap(kVm.asInstanceOf[Map[L, V]], edd.kVm))
+    case _ => throw DataDefinitionException("join not supported for Eager and non-Eager DataDefinition objects")
+  }
 }
 
 /**
   * Case Class which implements DataDefinition[K, W] and which is based on a Map[K,V] and a function V => W.
-  *
-  * CONSIDER implementing a filter also
   *
   * @param kVm        the map of key-value pairs which serve as the input to this LazyDD
   * @param f          a function which will transform the key-value pairs
@@ -84,7 +130,7 @@ trait DataDefinition[K, V] extends (() => Future[Map[K, V]]) {
   * @tparam V the input value type
   * @tparam W the output value type
   */
-case class LazyDD[K, V, L, W: Monoid](kVm: Map[K, V], f: ((K, V)) => (L, W))(partitions: Int = 2)(implicit context: DDContext) extends DataDefinition[L, W] {
+case class LazyDD[K, V, L, W: Monoid](kVm: Map[K, V], f: ((K, V)) => (L, W))(partitions: Int = 2)(implicit context: DDContext) extends BaseDD[L, W] {
 
   private implicit val cfs: Config = context.config
   private implicit val sys: ActorSystem = context.system
@@ -117,22 +163,12 @@ case class LazyDD[K, V, L, W: Monoid](kVm: Map[K, V], f: ((K, V)) => (L, W))(par
     }
 
   /**
-    * Clean up any resources in the context of this LazyDD object
-    */
-  def clean(): Unit = context.clean()
-
-  /**
-    * Method to apply an aggregate (i.e. reduce) function to the result of invoking apply
+    * Method to filter this DataDefinition according to a predicate which takes a l-w tuple.
     *
-    * @param g the aggregation function
-    * @tparam X the return type
-    * @return the returned value wrapped in Future
+    * @param p the predicate which will yield a Boolean for a given l-w tuple.
+    * @return a new DataDefinition containing only those l-w pairs which satisfy the predicate p.
     */
-  def aggregate[X: Zero](g: (X, W) => X): Future[X] = for (kWm <- apply()) yield kWm.values.foldLeft(implicitly[Zero[X]].zero)(g)
-
-  def count: Future[Int] = for (kWm <- apply()) yield kWm.size
-
-  def filter(g: ((L, W)) => Boolean): DataDefinition[L, W] = LazyDD[K, V, L, W](kVm.filter(f andThen g), f)(partitions)
+  def filter(p: ((L, W)) => Boolean): DataDefinition[L, W] = LazyDD[K, V, L, W](kVm.filter(f andThen p), f)(partitions)
 
   /**
     * Join method to perform inner join.
@@ -146,7 +182,42 @@ case class LazyDD[K, V, L, W: Monoid](kVm: Map[K, V], f: ((K, V)) => (L, W))(par
     case ldd: LazyDD[K, X, M, X]@unchecked =>
       import LazyDD._
       LazyDD[K, (V, X), M, (W, X)](joinMap(kVm, ldd.kVm), joinFunction(f, ldd.f))(partitions)
+    case edd: EagerDD[M, X] => join(LazyDD[M, X, M, X](edd.kVm, identity)(partitions))
   }
+}
+
+/**
+  * Abstract base class which implements the generic DataDefinition[K, V].
+  *
+  * @param context a DDContext
+  * @tparam K the key type
+  * @tparam V the input value type
+  */
+abstract class BaseDD[K, V](implicit context: DDContext) extends DataDefinition[K, V] {
+
+  private implicit val ec: ExecutionContext = context.ec
+
+  /**
+    * Clean up any resources in the context of this LazyDD object
+    */
+  def clean(): Unit = context.clean()
+
+  /**
+    * Method to evaluate this DataDefintion and reduce the dimensionality of the result by ignoring the keys
+    * and aggregating the values according to the function xw_x.
+    *
+    * @param xv_x the aggregation function.
+    * @tparam X the underlying type of the result.
+    * @return an X value, wrapped in Future.
+    */
+  def reduce[X: Zero](xv_x: (X, V) => X): Future[X] = for (kVm <- apply()) yield kVm.values.foldLeft(implicitly[Zero[X]].zero)(xv_x)
+
+  /**
+    * Evaluate the number of elements in this DataDefinition
+    *
+    * @return the number of k-v pairs
+    */
+  def count: Future[Int] = for (kVm <- apply()) yield kVm.size
 }
 
 /**
@@ -219,7 +290,7 @@ object DataDefinition {
 }
 
 object LazyDD {
-  private def joinMap[K, V, W](map1: Map[K, V], map2: Map[K, W]): Map[K, (V, W)] = (for (key <- map1.keySet intersect map2.keySet) yield (key, (map1(key), map2(key)))).toMap
+  private[majabigwaduce] def joinMap[K, V, W](map1: Map[K, V], map2: Map[K, W]): Map[K, (V, W)] = (for (key <- map1.keySet intersect map2.keySet) yield (key, (map1(key), map2(key)))).toMap
 
   private def joinFunction[K, V, L, W, X, Y](f: ((K, V)) => (L, W), g: ((K, X)) => (L, Y)): ((K, (V, X))) => (L, (W, Y)) = {
     case (k, (v, x)) =>
@@ -227,3 +298,5 @@ object LazyDD {
       vKf._1 -> (vKf._2, g(k, x)._2)
   }
 }
+
+case class DataDefinitionException(str: String) extends Exception(str)
--- src/test/scala/com/phasmid/majabigwaduce/DataDefinitionSpec.scala
+++ src/test/scala/com/phasmid/majabigwaduce/DataDefinitionSpec.scala
@@ -41,7 +41,7 @@ class DataDefinitionSpec extends FlatSpec with Matchers with Futures with ScalaF
     // given
     val target = DataDefinition(Map("a" -> 1, "b" -> 2), 0)
     // when
-    val xf: Future[Int] = target.aggregate[Int](_ + _)
+    val xf: Future[Int] = target.reduce[Int](_ + _)
     // then
     whenReady(xf) { x => x should matchPattern { case 3 => } }
     target.clean()
@@ -51,7 +51,7 @@ class DataDefinitionSpec extends FlatSpec with Matchers with Futures with ScalaF
     // given
     val target = DataDefinition(Map("a" -> 1, "b" -> 2))
     // when
-    val xf: Future[Int] = target.aggregate[Int](_ + _)
+    val xf: Future[Int] = target.reduce[Int](_ + _)
     // then
     whenReady(xf) { x => x should matchPattern { case 3 => } }
     target.clean()
@@ -124,7 +124,7 @@ class DataDefinitionSpec extends FlatSpec with Matchers with Futures with ScalaF
     val target = DataDefinition(Map("a" -> 1, "b" -> 2, "c" -> 3), 0)
     val target2 = DataDefinition(Map("a" -> 2.1, "b" -> 3.1), 0)
     // when
-    val xf: Future[(Int,Double)] = target.join(target2).aggregate[(Int,Double)]((x,y) => (x._1+y._1,x._2+y._2))
+    val xf: Future[(Int,Double)] = target.join(target2).reduce[(Int,Double)]((x, y) => (x._1+y._1,x._2+y._2))
     // then
     whenReady(xf) { x => x should matchPattern { case (3,5.2) => } }
     target.clean()
@@ -135,7 +135,7 @@ class DataDefinitionSpec extends FlatSpec with Matchers with Futures with ScalaF
     val target = DataDefinition(Map("a" -> 1, "b" -> 2, "c" -> 3))
     val target2 = DataDefinition(Map("a" -> 2.1, "b" -> 3.1))
     // when
-    val xf: Future[(Int,Double)] = target.join(target2).aggregate[(Int,Double)]((x,y) => (x._1+y._1,x._2+y._2))
+    val xf: Future[(Int,Double)] = target.join(target2).reduce[(Int,Double)]((x, y) => (x._1+y._1,x._2+y._2))
     // then
     whenReady(xf) { x => x should matchPattern { case (3,5.2) => } }
     target.clean()
@@ -147,7 +147,7 @@ class DataDefinitionSpec extends FlatSpec with Matchers with Futures with ScalaF
     val target2 = DataDefinition(Map("a" -> 2.1, "b" -> 3.1), 0)
     val target3 = DataDefinition(Map("a" -> "Hello", "b" -> "World"), 0)
     // when
-    val xf: Future[((Int,Double),String)] = target.join(target2).join(target3).aggregate[((Int,Double),String)]((x,y) => ((x._1._1 + y._1._1,x._1._2+y._1._2),x._2+","+y._2))
+    val xf: Future[((Int,Double),String)] = target.join(target2).join(target3).reduce[((Int,Double),String)]((x, y) => ((x._1._1 + y._1._1,x._1._2+y._1._2),x._2+","+y._2))
     // then
     whenReady(xf) { x => x should matchPattern { case ((3,5.2),",Hello,World") => } }
     target.clean()
@@ -159,7 +159,7 @@ class DataDefinitionSpec extends FlatSpec with Matchers with Futures with ScalaF
     val target2 = DataDefinition(Map("a" -> 2.1, "b" -> 3.1))
     val target3 = DataDefinition(Map("a" -> "Hello", "b" -> "World"))
     // when
-    val xf: Future[((Int,Double),String)] = target.join(target2).join(target3).aggregate[((Int,Double),String)]((x,y) => ((x._1._1 + y._1._1,x._1._2+y._1._2),x._2+","+y._2))
+    val xf: Future[((Int,Double),String)] = target.join(target2).join(target3).reduce[((Int,Double),String)]((x, y) => ((x._1._1 + y._1._1,x._1._2+y._1._2),x._2+","+y._2))
     // then
     whenReady(xf) { x => x should matchPattern { case ((3,5.2),",Hello,World") =>
                                             case ((3,5.2),",World,Hello") => } }
@@ -171,7 +171,7 @@ class DataDefinitionSpec extends FlatSpec with Matchers with Futures with ScalaF
     val target = DataDefinition(Map("a" -> 1, "b" -> 2, "c" -> 3), 0)
     val target2 = DataDefinition(Map("a" -> 2.1, "b" -> 3.1), 0)
     // when
-    val xf: Future[(Int,Double)] = target.map(tupleLift(_ * 2)).join(target2).aggregate[(Int,Double)]((x,y) => (x._1+y._1,x._2+y._2))
+    val xf: Future[(Int,Double)] = target.map(tupleLift(_ * 2)).join(target2).reduce[(Int,Double)]((x, y) => (x._1+y._1,x._2+y._2))
     // then
     whenReady(xf) { x => x should matchPattern { case (6,5.2) => } }
     target.clean()
@@ -182,7 +182,18 @@ class DataDefinitionSpec extends FlatSpec with Matchers with Futures with ScalaF
     val target = DataDefinition(Map("a" -> 1, "b" -> 2, "c" -> 3))
     val target2 = DataDefinition(Map("a" -> 2.1, "b" -> 3.1))
     // when
-    val xf: Future[(Int,Double)] = target.map(tupleLift(_ * 2)).join(target2).aggregate[(Int,Double)]((x,y) => (x._1+y._1,x._2+y._2))
+    val xf: Future[(Int,Double)] = target.map(tupleLift(_ * 2)).join(target2).reduce[(Int,Double)]((x, y) => (x._1+y._1,x._2+y._2))
+    // then
+    whenReady(xf) { x => x should matchPattern { case (6,5.2) => } }
+    target.clean()
+  }
+
+  it should "map/join/aggregate correctly with EagerDD" in {
+    // given
+    val target = DataDefinition(Map("a" -> 1, "b" -> 2, "c" -> 3))
+    val target2 = EagerDD(Map("a" -> 2.1, "b" -> 3.1))
+    // when
+    val xf: Future[(Int,Double)] = target.map(tupleLift(_ * 2)).join(target2).reduce[(Int,Double)]((x, y) => (x._1+y._1,x._2+y._2))
     // then
     whenReady(xf) { x => x should matchPattern { case (6,5.2) => } }
     target.clean()
@@ -250,5 +261,216 @@ class DataDefinitionSpec extends FlatSpec with Matchers with Futures with ScalaF
     whenReady(mf) { m => m.toSeq.size shouldBe 2 }
     target.clean()
   }
+
+  behavior of "EagerDD of Map"
+  it should "apply correctly with single partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2))
+    // when
+    val mf: Future[Map[String, Int]] = target()
+    // then
+    whenReady(mf) { m => m.toSeq should matchPattern { case Seq(("a", 1), ("b", 2)) => } }
+    target.clean()
+  }
+
+  it should "apply correctly with multiple partitions" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2))
+    // when
+    val mf: Future[Map[String, Int]] = target()
+    // then
+    import scala.concurrent.duration._
+    implicit val timeout: Timeout = Timeout(5 seconds)
+    whenReady(mf) { m => m.toSeq.size shouldBe 2 }
+    target.clean()
+  }
+
+  it should "aggregate correctly with single partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2))
+    // when
+    val xf: Future[Int] = target.reduce[Int](_ + _)
+    // then
+    whenReady(xf) { x => x should matchPattern { case 3 => } }
+    target.clean()
+  }
+
+  it should "aggregate correctly with multiple partitions" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2))
+    // when
+    val xf: Future[Int] = target.reduce[Int](_ + _)
+    // then
+    whenReady(xf) { x => x should matchPattern { case 3 => } }
+    target.clean()
+  }
+
+  it should "count correctly with single partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2))
+    // when
+    val xf: Future[Int] = target.count
+    // then
+    whenReady(xf) { x => x should matchPattern { case 2 => } }
+    target.clean()
+  }
+
+  it should "count correctly with multiple partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2))
+    // when
+    val xf: Future[Int] = target.count
+    // then
+    whenReady(xf) { x => x should matchPattern { case 2 => } }
+    target.clean()
+  }
+
+  it should "filter correctly with single partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2))
+    // when
+    val xf: Future[Int] = target.filter(x => x._1 == "a").count
+    // then
+    whenReady(xf) { x => x should matchPattern { case 1 => } }
+    target.clean()
+  }
+
+  it should "filter correctly with multiple partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2))
+    // when
+    val xf: Future[Int] = target.filter(x => x._1 == "a").count
+    // then
+    whenReady(xf) { x => x should matchPattern { case 1 => } }
+    target.clean()
+  }
+
+  it should "join/count correctly with single partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2))
+    val target2 = EagerDD(Map("a" -> 2.1, "c" -> 3.1))
+    // when
+    val xf: Future[(Int)] = target.join(target2).count
+    // then
+    whenReady(xf) { x => x should matchPattern { case 1 => } }
+    target.clean()
+  }
+
+  it should "join/count correctly with multiple partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2))
+    val target2 = EagerDD(Map("a" -> 2.1, "c" -> 3.1))
+    // when
+    val xf: Future[(Int)] = target.join(target2).count
+    // then
+    whenReady(xf) { x => x should matchPattern { case 1 => } }
+    target.clean()
+  }
+
+  it should "join/aggregate correctly with single partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2, "c" -> 3))
+    val target2 = EagerDD(Map("a" -> 2.1, "b" -> 3.1))
+    // when
+    val xf: Future[(Int,Double)] = target.join(target2).reduce[(Int,Double)]((x, y) => (x._1+y._1,x._2+y._2))
+    // then
+    whenReady(xf) { x => x should matchPattern { case (3,5.2) => } }
+    target.clean()
+  }
+
+  it should "join/aggregate correctly with multiple partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2, "c" -> 3))
+    val target2 = EagerDD(Map("a" -> 2.1, "b" -> 3.1))
+    // when
+    val xf: Future[(Int,Double)] = target.join(target2).reduce[(Int,Double)]((x, y) => (x._1+y._1,x._2+y._2))
+    // then
+    whenReady(xf) { x => x should matchPattern { case (3,5.2) => } }
+    target.clean()
+  }
+
+  it should "join/chain/aggregate correctly with single partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2, "c" -> 3))
+    val target2 = EagerDD(Map("a" -> 2.1, "b" -> 3.1))
+    val target3 = EagerDD(Map("a" -> "Hello", "b" -> "World"))
+    // when
+    val xf: Future[((Int,Double),String)] = target.join(target2).join(target3).reduce[((Int,Double),String)]((x, y) => ((x._1._1 + y._1._1,x._1._2+y._1._2),x._2+","+y._2))
+    // then
+    whenReady(xf) { x => x should matchPattern { case ((3,5.2),",Hello,World") => } }
+    target.clean()
+  }
+
+  it should "join/chain/aggregate correctly with multiple partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2, "c" -> 3))
+    val target2 = EagerDD(Map("a" -> 2.1, "b" -> 3.1))
+    val target3 = EagerDD(Map("a" -> "Hello", "b" -> "World"))
+    // when
+    val xf: Future[((Int,Double),String)] = target.join(target2).join(target3).reduce[((Int,Double),String)]((x, y) => ((x._1._1 + y._1._1,x._1._2+y._1._2),x._2+","+y._2))
+    // then
+    whenReady(xf) { x => x should matchPattern { case ((3,5.2),",Hello,World") =>
+    case ((3,5.2),",World,Hello") => } }
+    target.clean()
+  }
+
+  it should "map/join/aggregate correctly with single partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2, "c" -> 3))
+    val target2 = EagerDD(Map("a" -> 2.1, "b" -> 3.1))
+    // when
+    val xf: Future[(Int,Double)] = target.map(tupleLift(_ * 2)).join(target2).reduce[(Int,Double)]((x, y) => (x._1+y._1,x._2+y._2))
+    // then
+    whenReady(xf) { x => x should matchPattern { case (6,5.2) => } }
+    target.clean()
+  }
+
+  it should "map/join/aggregate correctly with multiple partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2, "c" -> 3))
+    val target2 = EagerDD(Map("a" -> 2.1, "b" -> 3.1))
+    // when
+    val xf: Future[(Int,Double)] = target.map(tupleLift(_ * 2)).join(target2).reduce[(Int,Double)]((x, y) => (x._1+y._1,x._2+y._2))
+    // then
+    whenReady(xf) { x => x should matchPattern { case (6,5.2) => } }
+    target.clean()
+  }
+
+  //The join only works when key is never changed, this test won't work since key changes
+  ignore should "mapKeyAndValue/join/apply correctly with single partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2, "c" -> 3))
+    val target2 = DataDefinition(Map("a1" -> 2.1, "b1" -> 3.1), 0)
+    // when
+    val mf: Future[Map[String, Int]] = target.map(x => (x._1+"1",x._2 * 2)).apply()
+    val mjf: Future[Map[String, (Int,Double)]] = target.map(x => (x._1+"1",x._2 * 2)).join(target2).apply()
+    // then
+    whenReady(mf) { m => m.toSeq should matchPattern { case Seq(("a1", 2), ("b1", 4), ("c1", 6)) => } }
+    whenReady(mjf) { m => m.toSeq should matchPattern { case Seq(("a1", (2,2.1)), ("b1", (4,3.1))) => } }
+    target.clean()
+  }
+
+  it should "map/apply correctly with single partition" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2))
+    // when
+    val mf: Future[Map[String, Int]] = target.map(tupleLift(_ * 2)).apply()
+    // then
+    whenReady(mf) { m => m.toSeq should matchPattern { case Seq(("a", 2), ("b", 4)) => } }
+    target.clean()
+  }
+
+  it should "map/apply correctly with multiple partitions" in {
+    // given
+    val target = EagerDD(Map("a" -> 1, "b" -> 2))
+    // when
+    val mf: Future[Map[String, Int]] = target.map(tupleLift(_ * 2)).apply()
+    // then
+    import scala.concurrent.duration._
+    implicit val timeout: Timeout = Timeout(5 seconds)
+    whenReady(mf) { m => m.values.sum shouldBe 6 }
+    target.clean()
+  }
+
 
 }
